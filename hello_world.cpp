61d5f49b41a0d04bb9b51de0175a7419ed1c3a77
xypimr <kiselev.nik.v@gmail.com>
2022-03-13 18:45:50 +0300

    NEW hello_world.cpp! Now with name!

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,8 +1,12 @@
+
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main(){
-    cout << "Hello World!" << endl;
+    string name;
+    cin >> name;
+    cout << "Hello world from " << name << endl;
     return 0;
 }
