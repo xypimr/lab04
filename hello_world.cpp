957edcc38c17bdc9644d99e518b226241d868267
xypimr <kiselev.nik.v@gmail.com>
2022-03-13 20:31:41 +0300

    Added comments to code fix

--- hello_world.cpp
+++ hello_world.cpp
@@ -3,8 +3,8 @@
 
 
 int main(){
-    std::string name;
-    std::cin >> name;
-    std::cout << "Hello world from " << name << std::endl;
-    return 0;
+    std::string name; 
+    std::cin >> name; // Inputing name from console
+    std::cout << "Hello world from " << name << std::endl; // Outputing text with name
+    return 0; 
 }
