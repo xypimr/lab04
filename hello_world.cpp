fb02e472f5d740ba8ac50c0fdd6c80ad88df5d3b
Киселев Николай Владимирович <89978129+xypimr@users.noreply.github.com>
2022-03-19 15:50:22 +0300

    Update hello_world.cpp

--- hello_world.cpp
+++ hello_world.cpp
@@ -6,5 +6,6 @@ int main(){
     std::string name; 
     std::cin >> name; // Inputing name from console
     std::cout << "Hello world from " << name << std::endl; // Outputing text with name
+    // new comment for conflict
     return 0; 
 }
